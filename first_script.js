b6732c450c5e72cfb98a3f3f902f3f3c8726b729
Ramanujam122459 <rambairi20585@gmail.com>
2026-05-06 22:49:36 +0530

    added new features to the script

--- first_script.js
+++ first_script.js
@@ -1,7 +1,8 @@
-import http from 'k6/http';
-import {check, sleep} from 'k6';
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { html } from "k6/html";
 
 export default function () {
-    http.get('https://test.k6.io'); 
-    sleep(1);
+  http.get("https://test.k6.io");
+  sleep(1);
 }
