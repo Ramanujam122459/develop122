dd40e7e11fbd848571a52c152ae2054d78a545de
Ramanujam122459 <rambairi20585@gmail.com>
2026-05-06 22:53:07 +0530

    added new feature to the script

--- first_script.js
+++ first_script.js
@@ -1,7 +1,10 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
-import { html } from "k6/html";
 
+export const options = {
+  vus: 10,
+  duration: "30s",
+};
 export default function () {
   http.get("https://test.k6.io");
   sleep(1);
