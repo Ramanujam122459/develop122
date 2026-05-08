ea0d673a4179d0ba616f1f74cf2bd1d49f32260d
Ramanujam122459 <rambairi20585@gmail.com>
2026-05-08 14:30:37 +0530

    adding new files

--- first_script.js
+++ first_script.js
@@ -1,11 +1,21 @@
 import http from "k6/http";
-import { check, sleep } from "k6";
+import { sleep, check } from "k6";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
-  vus: 10,
-  duration: "30s",
+  vus: 1,
+  duration: "1s",
 };
+
 export default function () {
-  http.get("https://test.k6.io");
-  sleep(1);
+  const response = http.get("https://test.k6.io/");
+  check(response,{"is statycode 200 " : (r) => r.status === 400})
 }
+sleep(2);
+
+// export function handleSummary(data) {
+//   return {
+//     "ram.html": htmlReport(data),
+//     // "ram.csv" : exportSummaryToCSV(data),
+//   };
+// }
